aa222fda727be2229131e4d1a655f96224241b02
shlok-bhatia <shlokbhatia16@gmail.com>
2025-12-06 18:31:52 +0530

    aed

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
-import api from '../api/api';
+import api from '../api/api.js';
 import { useNavigate } from 'react-router-dom';
 
 const AuthContext = createContext();
@@ -7,7 +7,7 @@ const AuthContext = createContext();
 export function AuthProvider({ children }) {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
-    const navigate = useNavigate();
+    
 
     useEffect(() => {
         async function fetchUser() {
@@ -77,7 +77,7 @@ export function AuthProvider({ children }) {
     async function logout() {
         await api.post('/auth/logout');
         setUser(null);
-        if (navigate) navigate('/signin');
+        
     }
 
   const updateSubscription = (subscriptionData) => {
