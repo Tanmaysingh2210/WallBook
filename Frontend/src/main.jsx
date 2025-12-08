1a617c95593ced5a46c32ce7f4296bbd057ee950
shlok-bhatia <shlokbhatia16@gmail.com>
2025-12-08 13:14:55 +0530

    d

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,6 +4,7 @@ import './main.css'
 import App from './App.jsx'
 import { AuthProvider } from "./context/AuthContext.jsx";
 import "./styles/global.css";
+import "./index.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
