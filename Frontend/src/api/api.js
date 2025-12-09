2d7bc15eaa8bada318e8a0777579a8db0b1ae2f5
shlok-bhatia <shlokbhatia16@gmail.com>
2025-12-09 13:27:38 +0530

    Change API base URL to production server

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://wallbook-dr97.onrender.com",
   withCredentials: true,            
 });
 
